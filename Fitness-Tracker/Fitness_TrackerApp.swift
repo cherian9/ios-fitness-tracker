a1235b6b96b77f7066984894264914231096b930
Cherian Chirackal Joseph <c7cherianjoseph@gmail.com>
2026-06-15 14:11:14 +0100

    Initial Commit

---
+++ Fitness-Tracker/Fitness_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Fitness_TrackerApp.swift
+//  Fitness-Tracker
+//
+//  Created by Cherian Chirackal Joseph on 15/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct Fitness_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
